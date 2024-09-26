f6f6982ec99e7666285834c2ba4ba6a61f83eeb8
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2024-09-25 21:57:18 -0300

    Adicionado try catch em todo a funcao cadastrar animal

--- src/ong/main.java
+++ src/ong/main.java
@@ -66,19 +66,18 @@ public class main {
 		String dataNascimento = sc.nextLine();
 		int mes = Integer.parseInt(dataNascimento.substring(0, 2));
 		int ano = Integer.parseInt(dataNascimento.substring(3, 7));
-				
+
+		int opcaoEspecie = 0;
 		String especie = "";
-	    while (true) {
+	    while (opcaoEspecie != 1 && opcaoEspecie != 2) {
 	        System.out.println("Digite a espécie do animal 1) Gato 2) Cachorro: ");
 	        String opcao = sc.nextLine();
 	        try {
-	            int opcaoEspecie = Integer.parseInt(opcao);
+	            opcaoEspecie = Integer.parseInt(opcao);
 	            if (opcaoEspecie == 1) {
 	                especie = "gato";
-	                break;
 	            } else if (opcaoEspecie == 2) {
 	                especie = "cachorro";
-	                break;
 	            } else {
 	                System.out.println("Opção inválida, digite novamente.");
 	            }
@@ -90,44 +89,49 @@ public class main {
 		String porte ="";
 		String pelos ="";
 		if (especie.equalsIgnoreCase("cachorro")) {
-			int opcaoPorte = 0;
-			while (opcaoPorte != 1 && opcaoPorte != 2 && opcaoPorte != 3) {
-				System.out.println("Digite o porte do animal 1) Pequeno 2) Médio 3) Grande: ");
-				opcaoPorte = sc.nextInt();
-				if (opcaoPorte == 1) {
-					porte = "pequeno";
-				} else if (opcaoPorte == 2) {
-					porte = "medio";
-				} else if (opcaoPorte == 3) {
-					porte = "grande";
-				} else {
-					System.out.println("Opção inválida, digite novamente");
-				}
-			}
-			
-			
-		
+		    int opcaoPorte = 0;
+		    String entradaPorte = "";
+		    while (opcaoPorte != 1 && opcaoPorte != 2 && opcaoPorte != 3) {
+		        System.out.println("Digite o porte do animal 1) Pequeno 2) Médio 3) Grande: ");
+		        entradaPorte = sc.nextLine(); 
+		        try {
+		            opcaoPorte = Integer.parseInt(entradaPorte);
+		            if (opcaoPorte == 1) {
+		                porte = "pequeno";
+		            } else if (opcaoPorte == 2) {
+		                porte = "medio";
+		            } else if (opcaoPorte == 3) {
+		                porte = "grande";
+		            } else {
+		                System.out.println("Opção inválida, digite novamente");
+		            }
+		        } catch (NumberFormatException e) {
+		            System.out.println("Entrada inválida. Por favor, insira um número.");
+		        }
+		    }
 		} else if (especie.equalsIgnoreCase("gato")) {
 			System.out.println("Digite o padrão de pelagem do gato:  ");
 			pelos = sc.nextLine();
-		} else {
-			System.out.println("Opção inválida, digite novamente");
 		}
 
 		int opcaoSexo = 0;
 		String sexo = "";
-
-		
 		while (opcaoSexo != 1 && opcaoSexo != 2) {
 			System.out.println("Digite qual sexo do animal: 1) Fêmea 2) Macho");
-			opcaoSexo = sc.nextInt();
-			if (opcaoSexo == 1) {
-				sexo = "femea";
-			} else if (opcaoSexo == 2) {
-				sexo = "macho";
-			} else {
-				System.out.println("Opção inválida, digite novamente");
+			String entradaSexo = sc.nextLine();
+			try {
+				opcaoSexo = Integer.parseInt(entradaSexo);
+				if (opcaoSexo == 1) {
+					sexo = "femea";
+				} else if (opcaoSexo == 2) {
+					sexo = "macho";
+				} else {
+					System.out.println("Opção inválida, digite novamente");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Entrada inválida. Por favor, insira um número.");
 			}
+			
 
 		}
 		if(especie.equals("cachorro")) {
